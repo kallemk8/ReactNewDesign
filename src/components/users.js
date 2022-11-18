09b142938af0843d1b5ad7c45b5ff4c2b54ab738
Sreekanth Kallem <sreekanth@vitelglobal.com>
2022-11-18 22:03:42 +0530

    changes for push

--- src/components/users.js
+++ src/components/users.js
@@ -38,6 +38,8 @@ const Users = () => {
             "CompanyID":tempuser.CompanyID
         }
         if(!editID){  
+            
+           
             try {
                 const response = await axios.post('http://localhost:5000/users', req);
                 if(response.data.insertId){
@@ -49,6 +51,8 @@ const Users = () => {
                 console.error(error);
               }
         }else{
+            req.status = getValues("status")
+            req.Singin = getValues('Singin');
             delete req.password;
             try {
                 const response = await axios.put(`http://localhost:5000/users/${editID}`, req);
@@ -81,6 +85,7 @@ const Users = () => {
         setValue('Mobile', data.Mobile)
         setValue('Gender', data.Gender)
         setValue('dob', data.dob)
+        setValue('Singin', data.Singin)
         
 
         setValue('status', data.status)
@@ -120,11 +125,11 @@ const Users = () => {
                                 <td><span> {item.status === 1 ?  "Active" : "Inactive"}</span></td>
                                 <td>
                                     
-                                    <><button className="btn btn-primary" style={{marginRight:"15px"}} onClick={()=>{addUser(true); setEdited(item.ID); onloadDetails(item)}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    <><button className="btn btn-primary" style={{marginRight:"15px"}} onClick={()=>{addUser(true); setEdited(item.UserID); onloadDetails(item)}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                                   </svg> </button>
-                                    <button className="btn btn-danger" onClick={()=>{DeleteUser(item.ID)}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                    <button className="btn btn-danger" onClick={()=>{DeleteUser(item.UserID)}}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                                     </svg></button></>
                                 </td>
@@ -152,9 +157,24 @@ const Users = () => {
                         <td>
                             <input type="date" className="form-control" {...register("dob")} placeholder="dob" />
                         </td>
+
+                        {editID !== "" ? 
                         <td>
-                            <button className="btn btn-primary" onClick={()=>saveUser()}>Save</button> &nbsp;</td>
-                            <td>    <button className="btn btn-primary" onClick={()=>addUser(false)}>Cancel</button>
+                            <select className="form-control"  {...register("status")}  >
+                                <option value="">Select Status</option>
+                                <option value="1">Active</option>
+                                <option value="2">Inactive</option>
+                            </select>
+
+                            <select className="form-control"  {...register("Singin")}  >
+                                <option value="">Select Status</option>
+                                <option value="yes">Yes</option>
+                                <option value="No">No</option>
+                            </select>
+                        </td> : "" }
+                        <td>
+                            <button className="btn btn-primary" onClick={()=>saveUser()}>Save</button> &nbsp;
+                            <button className="btn btn-primary" onClick={()=>addUser(false)}>Cancel</button>
                         </td>
                     </tr>
                     :
